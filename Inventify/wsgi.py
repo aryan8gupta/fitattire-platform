f5b8248e0981dd004882ca76f8d2f9e7f8032ff9
aryan8gupta <aryan8sunny@gmail.com>
2025-05-27 20:29:50 +0530

    new changes again

--- Inventify/wsgi.py
+++ Inventify/wsgi.py
@@ -11,7 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import Inventify
+
+
 settings_module = 'Inventify.deployment' if 'https://inventory-management-app-hnbvgqaag3g2hyde.centralindia-01.azurewebsites.net/' in os.environ else 'Inventify.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', Inventify.deployment)
 
 application = get_wsgi_application()
